f12c91da348790c5119faad61aad682143bd2558
Chia-Codes <sanchian-l@hotmail.co.uk>
2026-02-02 18:39:35 +0000

    feat: bootstrap base + home extends base

--- core/views.py
+++ core/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from testimonials.models import Testimonial
+from timetable.models import Session
+
 
 def home(request):
-    return render(request, "home.html")
+    sessions = Session.objects.filter(is_active=True)[:6]
+    testimonials = Testimonial.objects.filter(is_featured=True)[:3]
+    context = {"sessions": sessions, "testimonials": testimonials}
+    return render(request, "home.html", context)
